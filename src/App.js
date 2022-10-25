4bc73f02e524f49d74930d7e6d07e2650c27e4e7
Danielle Drake <danielledrake@mail.com>
2022-10-25 19:33:06 +0200

    react routes setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
+
+  function Home() {
+    return (
+      <>
+        <main>
+          <h2>Welcome to the homepage!</h2>
+          <p>You can do this, I believe in you.</p>
+        </main>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </>
+    );
+  }
+  
+  function About() {
+    return (
+      <>
+        <main>
+          <h2>Who are we?</h2>
+          <p>
+            That feels like an existential question, don't you
+            think?
+          </p>
+        </main>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+      </>
+    );
+  }
+  
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +46,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Routes>
+        <Route path="/" element={<Home />} />  {/*  change HOME to the element you want to render */} 
+        <Route path="about" element={<About />} /> {/*  change ABOUT to the element you want to render */} 
+      </Routes>
+
+
+       
       </header>
     </div>
   );
