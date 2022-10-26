311a1580ed1f06589910e68eed8b766dbd6bc5d9
Danielle Drake <danielledrake@mail.com>
2022-10-26 20:47:34 +0200

    api fetch working

--- src/App.js
+++ src/App.js
@@ -1,62 +1,48 @@
 import logo from './logo.svg';
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import SignView from './components/SignView';
+import MadameView from './components/MadameView';
+
 
 function App() {
 
+  
   function Home() {
     return (
       <>
         <main>
-          <h2>Welcome to the homepage!</h2>
-          <p>You can do this, I believe in you.</p>
+          <h2>Welcome!</h2>
+          <p>You are in the right place at the right time.</p>
         </main>
         <nav>
-          <Link to="/about">About</Link>
+          <Link to="/about">Madame Zodiac</Link>
+          <br />
+          <Link to="/sign">Horoscope</Link>
         </nav>
       </>
     );
   }
-  
-  function About() {
-    return (
-      <>
-        <main>
-          <h2>Who are we?</h2>
-          <p>
-            That feels like an existential question, don't you
-            think?
-          </p>
-        </main>
-        <nav>
-          <Link to="/">Home</Link>
-        </nav>
-      </>
-    );
-  }
-  
-
-
-
+ 
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="https://pbs.twimg.com/profile_images/1394800170290843649/eSMhUxwe_400x400.jpg" className="App-logo" alt="logo" />
+        
+        <br/>
+        <br/>
+        <p>The Zodiac Healer </p>
 
         <Routes>
-        <Route path="/" element={<Home />} />  {/*  change HOME to the element you want to render */} 
-        <Route path="about" element={<About />} /> {/*  change ABOUT to the element you want to render */} 
-      </Routes>
-
-
-       
+        < Route path="/" element={<Home />} />
+        < Route path="/sign" element={<SignView />} />
+        < Route path="/about" element={<MadameView />} />
+        </Routes>
       </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/SignView.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { Link, Router } from "react-router-dom";
+import "./SignView.css";
+
+
+export default function SignView() {
+    let [sign, setSign] = useState("");
+    let [date, setDate] = useState("");
+    let [horoscope, setHoroscope] = useState("");
+    let [error, setError] = useState("");
+    let [luckyNumber, setLuckyNumber] = useState("");
+
+     
+    const handleSubmit = e => {
+        e.preventDefault();
+        getHoroscope();
+      };
+
+// create a function that will fetch the horoscope data from the API and set the state of the horoscope variable to the data returned from the API
+// display the horoscope data in the browser
+
+const getHoroscope = () => {
+    const url = `http://aztro.sameerkumar.website/?sign=${sign}&day=${date}`;
+    fetch(url, { method: "POST" })
+    .then(response => response.json()) // parse the JSON from the server
+    .then(data => { // this is the data from the response
+        setHoroscope(data); 
+        setLuckyNumber(data.lucky_number);
+        setError(""); // clear the error
+    }) 
+    .catch(error => {
+        setError(error);
+        setHoroscope("");
+    });
+};
+
+// create a form that will allow the user to select their sign and date and submit the form to get their horoscope from the api. Display the horoscope data in the browser
+
+    return (
+        <div className="SignView">
+            <h1>Horoscope</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="sign">Sign</label>
+                <select 
+                    id="sign"
+                    value={sign}
+                    onChange={e => setSign(e.target.value)}
+                    >
+                    <option value="">--Please choose an option--</option>
+                    <option value="aries">Aries</option>
+                    <option value="taurus">Taurus</option>
+                    <option value="gemini">Gemini</option>
+                    <option value="cancer">Cancer</option>
+                    <option value="leo">Leo</option>
+                    <option value="virgo">Virgo</option>
+                    <option value="libra">Libra</option>
+                    <option value="scorpio">Scorpio</option>
+                    <option value="sagittarius">Sagittarius</option>
+                    <option value="capricorn">Capricorn</option>
+                    <option value="aquarius">Aquarius</option>
+                    <option value="pisces">Pisces</option>
+                    </select>
+                    
+                    <label htmlFor="date">Date</label>
+                    <select 
+                    id="date"
+                    value={date}
+                    onChange={e => setDate(e.target.value)}
+                    >
+                        <option value="">--Please choose an option--</option>
+                        <option value="today">Today</option>
+                        <option value="tomorrow">Tomorrow</option>
+                        </select>
+                        <button type="submit">Submit</button>
+                        </form>
+
+   
+    <div className="horoscope_container">
+        {horoscope && (
+            <div>
+                <h2>{horoscope.current_date}</h2>
+                <p>{horoscope.description}</p>
+                <p>{horoscope.color}</p>
+                <p>{horoscope.mood}</p>
+                <p>{horoscope.lucky_number}</p>
+                <p>{horoscope.lucky_time}</p>
+                <p>{luckyNumber}</p>
+            </div> )}
+        {error && <p>{error}</p>}
+    </div>
+    <nav>
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/about">Madame Zodiac</Link>
+    </nav>
+    </div>
+    );
+
+    // create a function that will fetch the horoscope data from the API using POST and the 'sign' and 'date' submitted by the form
+    // display the horoscope data in the browser
+
+ 
+    }
+
+
+
+
+
+
+
+
+
+        
+
+                        
