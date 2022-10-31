52d026afd45da3414f6de66d62f06dc487f78529
Danielle Drake <danielledrake@mail.com>
2022-10-31 08:47:49 +0100

    updated app wit css and gallery

---
+++ public/src/components/AffirmationAnswers.js
@@ -0,0 +1,116 @@
+export default [
+    {
+      "id": 1,
+      "answer": "It is certain",
+    },
+    {
+      "id": 2,
+      "answer":"Without a doubt",
+    },
+  
+    {
+      "id": 3,
+      "answer":"You may rely on it",
+    }
+    ,
+    {
+      "id": 4,
+      "answer":"It is decidedly so",
+    },
+    {
+        "id": 5,
+        "answer":"Yes, definitely",
+    },
+    {
+        "id": 6,
+        "answer":"The stars are aligned",
+    },
+    {
+        "id": 7,
+        "answer":"The vibes are good!",
+    },
+    {
+       "id": 8,
+       "answer":"Signs point to yes",
+    },
+    {
+      "id": 9,
+      "answer":"The outcome is better than you could have imagined",
+   },
+
+
+       {
+        "id": 10,
+        "answer":"The signs are hazy, focus and try again",
+         },
+
+    
+    {   
+        "id": 11,
+        "answer":"The vibrations are good",
+    },
+
+    {
+        "id": 12,
+        "answer":"Trust your gut, it's usually right",
+    },
+
+    {   
+        "id": 13,
+        "answer":"There's something you're not seeing, try again",
+    },
+
+    {
+        "id": 14,
+        "answer":"You know the answer, trust yourself",
+    },
+
+    {
+        "id": 15,
+        "answer":"Concentrate and ask again",
+    },
+
+    {
+        "id": 16,
+        "answer":"Don't count on it",
+    },
+
+    {
+        "id": 17,
+        "answer":"There's a fork in the road, choose wisely",
+    },
+
+    {
+        "id": 18,
+        "answer":"The signs point to no",
+    },
+
+    {
+        "id": 19,
+        "answer":"The vibes are off, try again",
+    },
+
+    {
+        "id": 20,
+        "answer":"The stars are not aligned",
+    },
+
+    {
+        "id": 21,
+        "answer":"The outcome is not what you hoped for",
+    },
+
+    {
+        "id": 22,
+        "answer":"If you really want it, you'll find a way",
+    },
+
+    {
+        "id": 23,
+        "answer":"The answer was shown to you, but you didn't see it",
+    },
+
+
+
+
+    ]
---
+++ public/src/components/AffirmationData.js
@@ -0,0 +1,45 @@
+// Credit and thanks to this API data created by Tilde Ann Thurium  https://github.com/annthurium/affirmations/
+export default [
+  {
+    "id": 1,
+    "affirmation": "I believe in you",
+  },
+  {
+    "id": 2,
+    "affirmation":"I give myself permission to do what is right for me",
+  },
+
+  {
+    "id": 3,
+    "affirmation":"Don't forget to enjoy the journey",
+  }
+  ,
+  {
+    "id": 4,
+    "affirmation":"I believe in you",
+  },
+  {
+      "id": 5,
+      "affirmation":"You make a difference in the world by simply existing in it",
+  },
+  {
+      "id": 6,
+      "affirmation":"You're a smart cookie",
+  },
+  {
+      "id": 7,
+      "affirmation":"Struggling means you're learning",
+  },
+  {
+     "id": 8,
+     "affirmation":"Small progress is still progress",
+  },
+  {
+    "id": 9,
+    "affirmation":"Don't worry if you find flaws in your past creations, it's because you've evolved",
+ },
+ {
+   "id": 10,
+   "affirmation":"The past does not equal the future",
+  }
+  ]
